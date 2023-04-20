8666aa7fb860cf5880b0e6b3a1bacccb627d0702
Nantawat Laothong <nantawat.laothong001@umb.edu>
2023-04-19 21:05:19 -0400

    edited .env file

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 require('dotenv').config(); 
 const express = require('express');
-const PORT = 3000;
+const PORT = process.env.PORT | 7000;
 const app = express();
 const path =require('path');
 const ejsMate = require('ejs-mate');
